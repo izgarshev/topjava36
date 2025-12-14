9affcd7e2486282bb330076d92731b892d92cab3
izgarshev <izgarshev_p@mail.ru>
2025-12-14 19:45:38 +0300

    hw3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS users;
+drop table if exists meal;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -23,3 +24,13 @@ CREATE TABLE user_role
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+create table meal (
+    id integer primary key default nextval('global_seq'),
+    date_time timestamp default now() not null,
+    description varchar not null,
+    calories integer not null,
+    user_id integer not null,
+    foreign key (user_id) references users(id) on delete cascade
+);
+create unique index meals_unique_id_date_time_idx on meal(id, date_time);
